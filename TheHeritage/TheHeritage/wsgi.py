b080ae4e16774ff6b0a52564af3865e72f07c213
Norah Saeed <norahseedw@gmail.com>
2026-04-08 22:37:03 +0300

    add home page

---
+++ TheHeritage/TheHeritage/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for TheCulture project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TheHeritage.settings')
+
+application = get_wsgi_application()
