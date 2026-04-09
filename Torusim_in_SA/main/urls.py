5159dad985c9ec3dabf7ae49b68d9bc1fbd7c52d
Abdulmjeed Bojeir <AbdulmjeedJOB12@gmail.com>
2026-04-09 06:23:22 +0300

    home_page + destintaions_page

--- Torusim_in_SA/main/urls.py
+++ Torusim_in_SA/main/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'main'
 
 urlpatterns = [
-    path('', views.home_view, name='home_view.html' )
+    path('', views.home_view, name='home_view' ),
+    path('destinations/', views.destinations_view, name='destinations_view'),
+    path('cities/riyadh/', views.riyadh_view, name="riyadh_html")
 ]
--- Torusim_in_SA/main/views.py
+++ Torusim_in_SA/main/views.py
@@ -6,3 +6,12 @@ from django.http import HttpRequest, HttpResponse
 def home_view(request:HttpRequest):
     
     return render(request, 'main/home_view.html')
+
+
+def destinations_view(request:HttpRequest):
+
+    return render(request, 'main/destinations.html')
+
+def riyadh_view(request:HttpRequest):
+
+    return render(request, 'main/riyadh.html')
