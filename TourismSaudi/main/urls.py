1894f43cbf5fd94a0f08c0f5c0b658065c7da9fa
sadeem <sadeemalharbi21@example.com>
2026-04-10 15:54:06 +0300

    Simple Touches!

--- TourismSaudi/main/urls.py
+++ TourismSaudi/main/urls.py
@@ -9,13 +9,14 @@ urlpatterns = [
     path("Tabuk/", views.tabuk_view, name="tabuk_view"),
     path("Medina/", views.medina_view, name="medina_view"),
     path("Mecca/", views.mecca_view, name="mecca_view"),   
-    path("Khobar/", views.khobar_view, name="khobar_view"),
+    path("eastern/", views.eastern_view, name="eastern_view"),
     path("Jeddah/", views.jeddah_view, name="jeddah_view"),
     path("Jazan/", views.jazan_view, name="jazan_view"),
     path("Aseer/", views.aseer_view, name="aseer_view"),
     path("Aljouf/", views.aljouf_view, name="aljouf_view"),
     path("Alula/", views.alula_view, name="alula_view"),
     path("Altaif/", views.taif_view, name="taif_view"),
+    path("Alqassim/", views.alqassim_view, name="alqassim_view"),
     path("view/all", views.discover_view, name="discover_view"),
-
+    path("login/", views.login_view, name="login_view"),
 ]
--- TourismSaudi/main/views.py
+++ TourismSaudi/main/views.py
@@ -12,8 +12,8 @@ def riyadh_view(request):
 def jeddah_view(request):
     return render(request, 'main/jeddah.html')
 
-def khobar_view(request):
-    return render(request, 'main/khobar.html')
+def eastern_view(request):
+    return render(request, 'main/eastern.html')
 
 def medina_view(request):
     return render(request, 'main/medina.html')
@@ -39,5 +39,11 @@ def alula_view(request):
 def taif_view(request):
     return render (request, 'main/taif.html')
 
+def alqassim_view(request):
+    return render (request, "main/alqassim.html")
+
 def discover_view(request):
     return render (request, "main/discover.html")
+
+def login_view(request):
+    return render (request, "main/login.html")
