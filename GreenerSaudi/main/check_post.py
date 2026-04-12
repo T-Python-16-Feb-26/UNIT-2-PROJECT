eadc3c792dea5fc583661702706a2be0fc5345b4
Mohammed <mohammed@Mohammeds-MacBook-Pro.local>
2026-04-12 13:18:19 +0300

    Fixed styles

--- GreenerSaudi/main/check_post.py
+++ GreenerSaudi/main/check_post.py
@@ -2,8 +2,7 @@ import opennsfw2 as n2
 from better_profanity import profanity
 
 profanity.load_censor_words()
-
 print(profanity.contains_profanity("hey how we doin"))
-#score = n2.predict_image('C:\\Users\\Mohammed\\Desktop\\tuwaiq\\Unit-2\\UNIT-2-PROJECT\\GreenerSaudi\\main\\static\\img\\about-mgi.webp')
-# print(score)
+score = n2.predict_image('')
+print(score)
 
