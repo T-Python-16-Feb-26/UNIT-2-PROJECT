86b8d44c1f0c5824455b8da104f3bf9612ea21df
Wajn <wajnaq1@gmail.com>
2026-04-13 07:44:41 +0300

    updated

--- saudi_esports/core/urls.py
+++ saudi_esports/core/urls.py
@@ -1,17 +1,14 @@
 from django.urls import path
 from . import views
 
-app_name ="core"
+app_name = "core"
 
 urlpatterns = [
-    path('', views.home_view, name='home'),
-
-    path('about/', views.about_view, name='about'),
-
-    path('games/', views.games_view, name='games'),
-    path('teams/', views.teams_view, name='teams'),
-
-    path('infrastructure/', views.infrastructure_view, name='infrastructure'),
-    path('events/', views.events_view, name='events'),
-    path('timeline/', views.timeline_view, name='timeline'),
+    path('', views.home_view, name='home_view'),
+    path('about/', views.about_view, name='about_view'),
+    path('games/', views.games_view, name='games_view'),
+    path('events/', views.events_view, name='events_view'),
+    path('teams/', views.teams_view, name='teams_view'),
+    path('infrastructure/', views.infrastructure_view, name='infrastructure_view'),
+    path('timeline/', views.timeline_view, name='timeline_view'),
 ]
